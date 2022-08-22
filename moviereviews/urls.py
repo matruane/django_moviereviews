cb2f32941a3d632c115a97bf41a9830fb4a0be9f
Mat Ruane <mat@ruane.net.nz>
2022-08-22 19:51:18 +1200

    Add news app

--- moviereviews/urls.py
+++ moviereviews/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.conf.urls.static import static
 from django.conf import settings
-from django.urls import path
+from django.urls import path, include
 from movie import views as movieViews
 
 urlpatterns = [
@@ -24,6 +24,7 @@ urlpatterns = [
     path('', movieViews.home, name='home'),
     path('about/', movieViews.about, name='about'),
     path('signup/', movieViews.signup, name='signup'),
+    path('news/', include('news.urls')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
