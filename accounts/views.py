b2b66f8b79e310a539b35e3a0f0fc92cdd38f984
Mat Ruane <mat@ruane.net.nz>
2022-09-03 09:42:46 +1200

    Implement login/logout

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from .forms import UserCreateForm
 from django.contrib.auth.models import User
-from django.contrib.auth import login
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login, logout, authenticate
 from django.shortcuts import redirect
 from django.db import IntegrityError
 
@@ -20,3 +21,18 @@ def signupaccount(request):
     else:
       # Tell the user the passwords didn't match
       return render(request, 'signupaccount.html', {'form': UserCreateForm, 'error': 'Passwords did not match'})
+
+def logoutaccount(request):
+  logout(request)
+  return redirect('home')
+
+def loginaccount(request):
+  if request.method == 'GET':
+    return render(request, 'loginaccount.html', {'form': AuthenticationForm})
+  else:
+    user = authenticate(request, username=request.POST['username'], password=request.POST['password'])
+    if user is None:
+      return render(request, 'loginaccount.html', {'form': AuthenticationForm, 'error': 'Username and password did not match'})
+    else:
+      login(request, user)
+      return redirect('home')
