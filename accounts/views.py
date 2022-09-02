822b6732824cfcb40d0073cd5a58d65821645461
Mat Ruane <mat@ruane.net.nz>
2022-09-02 16:29:25 +1200

    Implement user creation

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,22 @@
 from django.shortcuts import render
-from django.contrib.auth.forms import UserCreationForm
+from .forms import UserCreateForm
+from django.contrib.auth.models import User
+from django.contrib.auth import login
+from django.shortcuts import redirect
+from django.db import IntegrityError
 
 def signupaccount(request):
-    form = UserCreationForm()
-    return render(request, 'signupaccount.html', {'form': form})
+  if request.method == 'GET':
+    return render(request, 'signupaccount.html', {'form': UserCreateForm})
+  else:
+    if request.POST['password1'] == request.POST['password2']:
+      try:
+        user = User.objects.create_user(request.POST['username'], password=request.POST['password1'])
+        user.save()
+        login(request, user)
+        return redirect('home')
+      except IntegrityError:
+        return render(request, 'signupaccount.html', {'form': UserCreateForm, 'error': 'That username has already been taken. Please choose a new username'})
+    else:
+      # Tell the user the passwords didn't match
+      return render(request, 'signupaccount.html', {'form': UserCreateForm, 'error': 'Passwords did not match'})
